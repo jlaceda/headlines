3bf61271c091fb0faaa2fae64342ea1f1e5aa1bc
John Michael Laceda <john.laceda@gmail.com>
2019-07-02 15:28:37 -0700

    add basic error handling to scraper.

--- routes/scrape.js
+++ routes/scrape.js
@@ -8,36 +8,42 @@ const models = require("../models");
 const baseUrl = "https://www.apnews.com";
 
 router.get("/", (_, res) => {
-  axios.get(baseUrl + "/apf-topnews").then(response => {
-    const $ = cheerio.load(response.data);
-    $(".FeedCard").each((_, el) => {
-      const $el = $(el);
-      const headline = $el
-        .children(".CardHeadline")
-        .children(".headline")
-        .text();
-      const contentElement = $el.children("a").filter((_, aTag) => {
-        return $(aTag).attr("data-key") === "story-link";
-      });
-      const summary = contentElement
-        .children(".content")
-        .children("p")
-        .text();
-      const link = baseUrl + contentElement.attr("href");
-      models.Article.create({
-        headline: headline,
-        summary: summary,
-        link: link
-      })
-        .then(article => {
-          console.log(`Article added: ${article}`);
-        })
-        .catch(err => {
-          console.log(err);
+  axios
+    .get(baseUrl + "/apf-topnews")
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      $(".FeedCard").each((_, el) => {
+        const $el = $(el);
+        const headline = $el
+          .children(".CardHeadline")
+          .children(".headline")
+          .text();
+        const contentElement = $el.children("a").filter((_, aTag) => {
+          return $(aTag).attr("data-key") === "story-link";
         });
+        const summary = contentElement
+          .children(".content")
+          .children("p")
+          .text();
+        const link = baseUrl + contentElement.attr("href");
+        models.Article.create({
+          headline: headline,
+          summary: summary,
+          link: link
+        })
+          .then(article => {
+            console.log(`Article added: ${article}`);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+      res.status(200).send("Scrape Complete.");
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("Scraping Error.");
     });
-    res.send("Scrape Complete");
-  });
 });
 
 module.exports = router;
