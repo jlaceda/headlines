cc0d12120917842158f43def8e35240146972b7a
John Michael Laceda <john.laceda@gmail.com>
2019-07-01 22:49:41 -0700

    minor changes to server startup.

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 require("dotenv").config();
 const express = require("express");
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const scrape = require("./routes/scrape");
 
 app.get("/", (req, res) => res.send("It Works!"));
+app.use("/scrape", scrape);
 
-// only start express server after theres a connection to mongodb
+// define mongodb connection
 mongoose.connect(
   // use uri from env when in production
   process.env.NODE_ENV === "production"
     ? process.env.MONGO_DB_PROD_URI
     : "mongodb://localhost/headlines",
   {
+    // following lines are just to prevent deprecation warnings
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
-  },
-  err => {
-    if (err !== null) {
-      console.log(err);
-      process.exit(1);
-    }
-    console.log("Successfully to connected to mongodb.");
-    app.listen(PORT, () => {
-      console.log(
-        `==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
-      );
-    });
   }
 );
+
+const db = mongoose.connection;
+db.on("error", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+// only start express server after theres a connection to mongodb
+db.once("open", () => {
+  // we're connected!
+  console.log("Successfully to connected to mongodb.");
+  app.listen(PORT, () => {
+    console.log(
+      `==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
+    );
+  });
+});
