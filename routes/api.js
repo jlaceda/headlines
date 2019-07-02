79c0d59b3d5c0cf5d0f0ce149dca57d984b60b7c
John Michael Laceda <john.laceda@gmail.com>
2019-07-02 01:16:21 -0700

    add comment to api route

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,31 @@ const models = require("../models");
 
 router.get("/articles", (req, res) => {
   models.Article.find({})
+    .populate("comments")
     .then(articles => {
       res.send(articles);
     })
     .catch(err => {
       console.log(err);
+    });
+});
+
+router.post("comment/:articleid", (req, res) => {
+  const newComment = req.body;
+  models.Comment.create(newComment)
+    .then(comment => {
+      return models.Article.findOneAndUpdate(
+        { _id: req.params.articleid },
+        { $push: { comments: comment._id } },
+        { new: true }
+      );
+    })
+    .then(article => {
+      res.json(article);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
     });
 });
 
